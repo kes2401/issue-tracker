3d1e097113a84176b6b06a4ed1b06243560aa0cd
Keith Scully <keithscully83@gmail.com>
2019-06-21 15:56:47 +0100

    Add templates directory path

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'issue_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
