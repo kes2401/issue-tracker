9c2c58c5fcb5b9f96caaf76128174f1fb91c9adf
Keith Scully <keithscully83@gmail.com>
2019-06-24 22:21:38 +0100

    Adjust URL path for accounts views , and add logout functionality and messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 
 def login(request):
     """ Return login page containing login form so that user can log into the application """
     return render(request, 'login.html')
+
+
+def logout(request):
+    """ Log the user out and redirect to index page """
+    auth.logout(request)
+    messages.success(request, 'You have been successfully logged out')
+    return redirect(reverse('index'))
--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import index
-from accounts.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('login/', login),
+    path('', index, name='index'),
+    path('accounts/', include('accounts.urls')),
 ]
