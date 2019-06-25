25a67299cc403e8ac132f6f3d1e3255c8ae45e44
Keith Scully <keithscully83@gmail.com>
2019-06-25 20:36:58 +0100

    set Authorisation on login and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 
 def login(request):
     """ Return login page containing the login form """
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == 'POST':
         login_form = UserLoginForm(request.POST)
         if login_form.is_valid():
@@ -15,6 +18,7 @@ def login(request):
                 auth.login(user=user, request=request)
                 messages.success(
                     request, 'You have been succesfully logged in!')
+                return redirect(reverse('index'))
             else:
                 messages.error(
                     request, 'Your username or password is incorrect')
@@ -23,6 +27,7 @@ def login(request):
     return render(request, 'login.html', {'login_form': login_form})
 
 
+@login_required
 def logout(request):
     """ Log the user out and redirect to index page """
     auth.logout(request)
