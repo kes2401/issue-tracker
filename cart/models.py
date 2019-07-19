4c92a675ee92332fb18c066c8daaad117d563db3
Keith Scully <keithscully83@gmail.com>
2019-07-19 23:08:24 +0100

    cart model amount changed to integerfield

--- cart/models.py
+++ cart/models.py
@@ -9,13 +9,20 @@ class CartContent(models.Model):
     def validate_gt_fifty(value):
         if value < 50:
             raise ValidationError(
-                _('%(value)s is not greater than 50.00'),
+                _('%(value)s is not greater than 50'),
+                params={'value': value},
+            )
+
+    def validate_lt_fifteenhundred(value):
+        if value > 1500:
+            raise ValidationError(
+                _('%(value)s cannot be greater than 1500'),
                 params={'value': value},
             )
 
     user = models.ForeignKey(User, default='Anonymous', on_delete=models.CASCADE)
     product = models.ForeignKey(Issue, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=5, decimal_places=2, default=50.00, validators=[validate_gt_fifty])
+    amount = models.IntegerField(default=50, validators=[validate_gt_fifty, validate_lt_fifteenhundred])
 
     def __str__(self):
         return self.user
