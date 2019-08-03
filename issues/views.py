beef48c999bfc5ff23edfa1b0fbe340830344846
Keith Scully <keithscully83@gmail.com>
2019-08-04 00:11:38 +0100

    Build issue search, filter and sorting functionality

--- issues/views.py
+++ issues/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.db.models.functions import Lower
+from django.db.models import Count
 from .forms import CreateIssue
 from .models import Issue, IssueComment, IssueVote
 from cart.forms import Cart_New_feature
@@ -14,6 +16,55 @@ def tracker(request):
     """ Renders the Tracker page that tracks and manages all bug reports and feature requests """
     bugs = Issue.objects.filter(issue_type='bug')
     features = Issue.objects.filter(issue_type='feature')
+
+    if request.method == 'POST':
+        if request.POST.get('bug-search') != None:
+            bug_search_term = request.POST.get('bug-search')
+            bugs = bugs.filter(title__icontains=bug_search_term)
+        elif request.POST.get('feature-search') != None:
+            feature_search_term = request.POST.get('feature-search')
+            features = features.filter(title__icontains=feature_search_term)
+        
+        if request.POST.get('bugStatus') == 'pending':
+            bugs = bugs.filter(status='pending')
+        elif request.POST.get('bugStatus') == 'in-progress':
+            bugs = bugs.filter(status='in progress')
+        elif request.POST.get('bugStatus') == 'closed':
+            bugs = bugs.filter(status='closed')
+
+        if request.POST.get('featureStatus') == 'pending':
+            features = features.filter(status='pending')
+        elif request.POST.get('featureStatus') == 'in-progress':
+            features = features.filter(status='in progress')
+        elif request.POST.get('featureStatus') == 'closed':
+            features = features.filter(status='closed')
+        
+        if request.POST.get('bugSort') == 'name-down':
+            bugs = bugs.order_by(Lower('title').asc())
+        elif request.POST.get('bugSort') == 'name-up':
+            bugs = bugs.order_by(Lower('title').desc())
+        elif request.POST.get('bugSort') == 'votes-down':
+            bugs = bugs.annotate(num_votes=Count('issuevote')).order_by('-num_votes')
+        elif request.POST.get('bugSort') == 'votes-up':
+            bugs = bugs.annotate(num_votes=Count('issuevote')).order_by('num_votes')
+        elif request.POST.get('bugSort') == 'comments-down':
+            bugs = bugs.annotate(num_comments=Count('issuecomment')).order_by('-num_comments')
+        elif request.POST.get('bugSort') == 'comments-up':
+            bugs = bugs.annotate(num_comments=Count('issuecomment')).order_by('num_comments')
+        
+        if request.POST.get('featureSort') == 'name-down':
+            features = features.order_by(Lower('title').asc())
+        elif request.POST.get('featureSort') == 'name-up':
+            features = features.order_by(Lower('title').desc())
+        elif request.POST.get('featureSort') == 'votes-down':
+            features = features.annotate(num_votes=Count('issuevote')).order_by('-num_votes')
+        elif request.POST.get('featureSort') == 'votes-up':
+            features = features.annotate(num_votes=Count('issuevote')).order_by('num_votes')
+        elif request.POST.get('featureSort') == 'comments-down':
+            features = features.annotate(num_comments=Count('issuecomment')).order_by('-num_comments')
+        elif request.POST.get('featureSort') == 'comments-up':
+            features = features.annotate(num_comments=Count('issuecomment')).order_by('num_comments')
+
     comments = IssueComment.objects.all()
     votes = IssueVote.objects.all()
     for bug in bugs:
---
+++ static/js/tracker.js
@@ -0,0 +1,15 @@
+$('.bug-status').change(function () {
+    $('#bug-filter-form').submit();
+})
+
+$('.bug-sort').change(function () {
+    $('#bug-filter-form').submit();
+})
+
+$('.feature-status').change(function () {
+    $('#feature-filter-form').submit();
+})
+
+$('.feature-sort').change(function () {
+    $('#feature-filter-form').submit();
+})
