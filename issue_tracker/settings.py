cd20ff6c86e1ec7a9c5307fc96ac140a708d5586
Keith Scully <keithscully83@gmail.com>
2019-08-08 23:26:51 +0100

    edit travis file

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -106,7 +106,6 @@ else:
             'TEST': {
                 'ENGINE': 'django.db.backends.sqlite3',
                 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-                'TEMPLATE': 'DEFAULT',
             },
         }
         
